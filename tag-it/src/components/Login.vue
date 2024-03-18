d46e4537f2a1d4c27a01fcf4a9b8af7801d11b34
Mervyn Seah <6378ms@gmail.com>
2024-03-18 16:46:03 +0800

    login base interface

---
+++ tag-it/src/components/Login.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script>
+import firebaseApp from "../firebase.js";
+import { getFirestore } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
+
+const db = getFirestore(firebaseApp);
+</script>
--- tag-it/src/firebase.js
+++ tag-it/src/firebase.js
@@ -1,6 +1,15 @@
 import { initializeApp } from "firebase/app";
 require("dotenv").config();
 
+import {
+    getAuth,
+    connectAuthEmulator,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut
+} from "firebase/auth";
+
 const firebaseConfig = {
     // stuffs here
     apiKey: keys.env.apiKey,
@@ -12,5 +21,52 @@ const firebaseConfig = {
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
 
 export default firebaseApp;
+connectAuthEmulator(auth, "http://localhost:9099");
+
+const loginEmailPassword = async () => {
+    const loginEmail = txtEmail.value;
+    const loginPassowrd = txtPassword.value;
+
+    const userCredential = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassowrd
+    );
+    console.log(userCredential.user); // remove this line
+};
+
+const createAccount = async () => {
+    const loginEmail = txtEmail.value;
+    const loginPassowrd = txtPassword.value;
+
+    const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassowrd
+    );
+};
+
+const monitorAuthState = async() => {
+    onAuthStateChanged(auth, user => {
+        if (user) => {
+            console.log(user)
+            showApp();
+            showLoginState(user);
+
+            hideLoginError();
+        } 
+        else {
+            showLoginForm();
+            //lblAuthState.innterHTML = "You're not logged in"
+        }
+    })
+}
+
+const logout = async() => {
+    await signOut(auth);
+}
+
+btnLogout.addEventListener("click", logout);

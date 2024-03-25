9606665ec32581cf8155818acd4d9c7d67a7fdfa
juschew03 <juschew03@gmail.com>
2024-03-25 16:32:54 +0800

    added test router view

--- tag-it/src/App.vue
+++ tag-it/src/App.vue
@@ -4,6 +4,8 @@
     <!-- <Login /> -->
     <Login />
     <SignUp />
+
+    <router-view />
 </template>
 
 <script>
--- tag-it/src/main.js
+++ tag-it/src/main.js
@@ -2,7 +2,12 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
+import router from "./router"
 import Login from "./components/Login.vue";
 import SignUp from "./components/SignUp.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App)
+
+app.use(router)
+
+app.mount("#app")

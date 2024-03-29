4a1cc2cf32584a824de335f62793eb509bca9939
Mervyn Seah <6378ms@gmail.com>
2024-03-29 17:44:57 +0800

    Update index.js
    fixed import line

--- tag-it/src/router/index.js
+++ tag-it/src/router/index.js
@@ -1,12 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-
 import Home from "@/views/Home.vue";
 import Login from "@/components/Login.vue";
 import SignUp from "@/components/SignUp.vue";
 import ForgotPassword from "@/views/ForgotPassword.vue";
 import ResetPassword from "@/views/ResetPassword.vue";
-
+import EditProfile from "@/views/EditProfile.vue";
 
 const routes = [
     {
@@ -25,22 +24,21 @@ const routes = [
         component: SignUp,
     },
     {
-    path: "/forgot-password",
-    name: "ForgotPassword",
-    component: ForgotPassword,
-  },
-  {
-    path: "/reset-password",
-    name: "ResetPassword",
-    component: ResetPassword,
-  },
+        path: "/forgot-password",
+        name: "ForgotPassword",
+        component: ForgotPassword,
+    },
+    {
+        path: "/reset-password",
+        name: "ResetPassword",
+        component: ResetPassword,
+    },
 
-  {
-    path: "/edit-profile",
-    name: "EditProfile",
-    component: EditProfile,
-  }
-
+    {
+        path: "/edit-profile",
+        name: "EditProfile",
+        component: EditProfile,
+    },
 ];
 
 const router = createRouter({ history: createWebHistory(), routes });

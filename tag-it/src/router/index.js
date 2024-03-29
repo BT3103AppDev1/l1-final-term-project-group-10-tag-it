4fef2b34561842afb4cf6276d7b4c6013707687e
Mervyn Seah <6378ms@gmail.com>
2024-03-29 18:16:38 +0800

    Revert "Navbarfooter"

--- tag-it/src/router/index.js
+++ tag-it/src/router/index.js
@@ -1,41 +1,46 @@
 import { createRouter, createWebHistory } from "vue-router";
-// import Login from "@/components/Login.vue";
+
 import Home from "@/views/Home.vue";
-import AllTags from '@/views/AllTags.vue';
-import Login from '@/components/Login.vue';
-import AllTagsComp from '@/components/AllTags.vue';
-import Calendar from "@/views/Calendar.vue";
-import ExpandedTagEntry from '@/components/ExpandedTagEntry.vue';
-import MiscellaneousTags from '@/components/MiscellaneousTags.vue';
-
-
+import Login from "@/components/Login.vue";
+import SignUp from "@/components/SignUp.vue";
+import ForgotPassword from "@/views/ForgotPassword.vue";
+import ResetPassword from "@/views/ResetPassword.vue";
+import EditProfile from "@/views/EditProfile.vue";
 
 const routes = [
     {
         path: "/",
         name: "Login",
-        component: Login, 
-      },
+        component: Login,
+    },
     {
         path: "/home",
         name: "Home",
         component: Home,
     },
     {
-        path: "/alltags",
-        name: "AllTags",
-        component: AllTags, AllTagsComp,
+        path: "/signUp",
+        name: "SignUp",
+        component: SignUp,
     },
     {
-      path: "/calendar",
-      name: "Calendar",
-      component: Calendar, ExpandedTagEntry, MiscellaneousTags,
-  }
+        path: "/forgot-password",
+        name: "ForgotPassword",
+        component: ForgotPassword,
+    },
+    {
+        path: "/reset-password",
+        name: "ResetPassword",
+        component: ResetPassword,
+    },
+
+    {
+        path: "/edit-profile",
+        name: "EditProfile",
+        component: EditProfile,
+    },
 ];
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes
-});
+const router = createRouter({ history: createWebHistory(), routes });
 
 export default router;
--- tag-it/src/views/Home.vue
+++ tag-it/src/views/Home.vue
@@ -1,53 +1,16 @@
 <template>
-    <div class="full-page-bg">
-        <div class="texts">
-            <h1 class="welcome-msg">
-                Welcome Mervyn! 
-                <!-- name will be accessed from the database -->
-            </h1>
-            <h1 class="tags-msg">
-                Your have 4 Tags today! 
-                <!-- number of Tags will be accessed from the database -->
-            </h1>
-        </div>
-        <Footer2/>
-    </div>
+    this is home page
+    <AllTags />
 </template>
-
 <script>
-import Footer2 from '@/components/Footer2.vue';
+// ===== LOGIC =====
+// if loggedin -> show dashboard
+// if NOT loggedin -> show login
+
+import AllTags from "@/components/AllTags.vue";
 export default {
     name: "Home",
-    components: {
-        Footer2,
-    }
-}
+    components: { AllTags },
+};
 </script>
-
-<style>
-
-.texts {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 60vh;
-}
-.welcome-msg {
-    color: #fff;
-    font-size: 7vh;
-    margin-top: 30px;
-    margin-bottom: 40px;
-}
-
-.tags-msg {
-    color: #000;
-    font-size: 7vh;
-}
-
-.full-page-bg {
-  height: 100vh; /* make sure it covers the full viewport height */
-  /* background-color: #343a40;  */
-  background-image: linear-gradient(#fff, #0A42AD);
-} 
-</style>
+<style></style>
